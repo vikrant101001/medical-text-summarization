17e367ecd0361bad8b0194b6bdc08724cfaece79
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2023-04-10 19:25:50 +0530

    adding questiongenerate
    First version of question generate

--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -9,3 +9,22 @@ def summarize(prompt):
         temperature=.5,
         max_tokens=1000,
     )["choices"][0]["text"]
+ 
+def questiongenerate(prompt):
+    prompt = "Generate questions based on the following text:\n" + text + "\n\n1. "
+    completions = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=1024,
+        n=10,
+        stop=None,
+        temperature=0.5,
+    )
+
+    questions = []
+    for choice in completions.choices:
+        question = choice.text.strip()
+        if question:
+            questions.append(question)
+
+    return questions
