b6144d6cc89252405cd58529bc1482bdab5a6f8e
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2023-04-11 16:59:00 +0530

    Update functions.py

--- text_summarizer/functions.py
+++ text_summarizer/functions.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 def summarize(prompt):
     augmented_prompt = f"summarize this text: {prompt}"
+    st.session_state["pr"]="Get a medical based summary of this text"
     st.session_state["summary"] = openai.Completion.create(
         model="text-davinci-003",
         prompt=augmented_prompt,
