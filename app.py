1c989b2730c79d330cff6e25f703b2f919d84027
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2023-04-14 20:28:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,21 +24,28 @@ st.title("Medical Text Summarizer")
 
 input_text = st.text_area(label="Enter full text:", value="", height=250)
 
-st.button(
-    "Medical Summary",
-    on_click=summarize,
-    kwargs={"prompt": input_text},
-)
-st.button(
-    "Key points",
-    on_click=findkeypoints,
-    kwargs={"prompt": input_text},
-)
-st.button(
-    "question generation",
-    on_click=generatequestions,
-    kwargs={"prompt": input_text},
-)
+col1, col2, col3 = st.columns([1,1,1])
+
+with col1:
+    st.button(
+        "Medical Summary",
+        on_click=summarize,
+        kwargs={"prompt": input_text},
+    )
+ 
+with col2:
+    st.button(
+        "Key points",
+        on_click=findkeypoints,
+        kwargs={"prompt": input_text},
+    )
+    
+with col3:
+    st.button(
+        "question generation",
+        on_click=generatequestions,
+        kwargs={"prompt": input_text},
+    )
 
 Prompt_field = st.text_area(label="Prompt:", value=st.session_state["pr"], height=100)
 summary = st.text_area(label="Summary:", value=st.session_state["summary"], height=200)
